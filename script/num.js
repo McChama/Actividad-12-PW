abbe7eb4b76890179cf170ae274b93fc47131445
Emiliano Vázquez Banda <61950109+McChama@users.noreply.github.com>
2020-11-05 19:35:37 -0600

    Add files via upload

---
+++ script/num.js
@@ -0,0 +1,22 @@
+function Validar(){
+    var Inputs = document.getElementsByClassName("input");
+     
+    for(i=0;i<Inputs.length;i++){
+        if(Inputs[i].value == 0){
+            alert("Llena todos los espacios");
+            return;
+        }
+    }
+    return Calcular(Inputs);
+}
+function Calcular(Inputs){
+    var N1 = parseFloat(Inputs[0].value);
+    var N2 = parseFloat(Inputs[1].value);
+    var N3 = parseFloat(Inputs[2].value);
+    var May;
+
+    May = Math.max(N1,N2,N3);
+
+    document.getElementById("Mayor").innerHTML = ("El numero mayor es: "+May);
+
+}
